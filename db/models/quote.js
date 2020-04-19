cf33760e4f15b754a942ccd3f8e07012fc65a20f
Reuben Sarkar <rsreubens96@gmail.com>
2020-04-19 13:51:31 +0100

    removed public

---
+++ db/models/quote.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const QuotesSchema = new mongoose.Schema({
+    quotee: {
+        type: String,
+        trim: true,
+        required:true
+    },
+    body: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    dateQuoted: {
+        type: Date,
+        default: Date.now
+    },
+    dateCreated: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const quote = mongoose.model('Quote', QuotesSchema);
+
+module.exports = quote;
