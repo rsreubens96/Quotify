00b92c4a5e0d885433867594883bd9e55c82a860
ChangoTheBeast <33813476+BoboTheNameless@users.noreply.github.com>
2020-07-17 00:50:49 +0100

    Changed date format
    Too much work

--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -10,7 +10,44 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
 import { HomeComponent } from './components/home/home.component';
 import { ModalComponent } from './components/add_quote_modal/modal.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgbDateParserFormatter, NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
+import { NgbDateCustomParserFormatter } from './components/add_quote_modal/modal.customDateParser';
+
+// function isNumber(arg: any) {
+//   return typeof arg === 'number';
+// }
+//
+// function padNumber(n: number) {
+//   return n < 10 ? '0' + n.toString(10) : n.toString(10);
+// }
+//
+// function toInteger(arg: string) {
+//   return parseInt(arg);
+// }
+//
+// @Injectable()
+// export class NgbDateCustomParserFormatter extends NgbDateParserFormatter {
+//   parse(value: string): NgbDateStruct {
+//     if (value) {
+//       const dateParts = value.trim().split('-');
+//       if (dateParts.length === 1 && isNumber(dateParts[0])) {
+//         return {day: toInteger(dateParts[0]), month: null, year: null};
+//       } else if (dateParts.length === 2 && isNumber(dateParts[0]) && isNumber(dateParts[1])) {
+//         return {day: toInteger(dateParts[0]), month: toInteger(dateParts[1]), year: null};
+//       } else if (dateParts.length === 3 && isNumber(dateParts[0]) && isNumber(dateParts[1]) && isNumber(dateParts[2])) {
+//         return {day: toInteger(dateParts[0]), month: toInteger(dateParts[1]), year: toInteger(dateParts[2])};
+//       }
+//     }
+//     return null;
+//   }
+//
+//   format(date: NgbDateStruct): string {
+//     return date ?
+//         `${isNumber(date.day) ? padNumber(date.day) : ''}-${isNumber(date.month) ? padNumber(date.month) : ''}-${date.year}` :
+//         '';
+//   }
+// }
 
 @NgModule({
   declarations: [
@@ -29,7 +66,9 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
     FormsModule,
     ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    {provide: NgbDateParserFormatter, useClass: NgbDateCustomParserFormatter}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
